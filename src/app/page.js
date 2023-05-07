a99f80c3fdcd1fe542490129d331295252417f43
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2023-05-07 13:50:39 +0800

    added experiences

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Experiences from './components/experiences';
+
 export default function Home() {
   return (
     <main className="">
-      <nav className="flex w-full py-4 px-4 justify-between bg-neutral-900" >
+      <nav className="flex w-full py-4 px-10 justify-between bg-neutral-100" >
         <div className="flex">
-          <a href="#" className="text-2xl hover:text-blue-800">Ayar Hlaine</a>
+          <a href="#" className="text-2xl hover:text-blue-800 uppercase">Ayar Hlaine</a>
          </div>
          <div>
           <a href="#" className="mr-4">Github</a>
@@ -11,28 +13,34 @@ export default function Home() {
          </div>
       </nav>
       {/* Hero Section */}
-      <div className="hero-section flex w-full flex-row px-10 py-2 h-96">
+      <div className="hero-section flex w-full flex-row px-10 py-10">
         <div className="hero-left-section basis-1/2">
-          <h1 className="text-2xl">Nice to meet you!</h1>
-          <h1 className="text-2xl">I am Ayar Hlaine</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid dolorum velit minus laborum iusto sequi blanditiis consequatur tempore hic nam totam repellendus sunt quae, unde soluta at, culpa veniam in.</p>
+          <h1 className="text-6xl">Nice to meet you!</h1>
+          <h1 className="text-7xl mt-5">I am Ayar Hlaine</h1>
+          <p className='mt-10 text-lg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid dolorum velit minus laborum iusto sequi blanditiis consequatur tempore hic nam totam repellendus sunt quae, unde soluta at, culpa veniam in.</p>
         </div>
         <div className="hero-right-section basis-1/2">
           Image
         </div>
       </div>
       {/* Experiences */}
-      <section className="h-96 px-10 py-2">
-        <h1 className="text-2xl">Experiences</h1>
+      <section className="px-10 py-2">
+        <h1 className="text-lg uppercase">What I have done so far</h1>
+        <h1 className="text-7xl">Work Experience.</h1>
+        <div className='flex flex-col mt-20'>
+          <Experiences />
+        </div>
       </section>
       {/* Projects */}
       <section className="h-96 px-10 py-2">
-        <h1 className="text-2xl">Projects</h1>
+        <h1 className="text-lg uppercase">My Work</h1>
+        <h1 className="text-7xl">Projects.</h1>
       </section>
       <footer>
         {/* Contact Form */}
         <div className="h-96 px-10 py-2">
-          <h1 className="text-2xl">Contact</h1>
+          <h1 className="text-lg uppercase">Get In Touch</h1>
+          <h1 className="text-7xl">Contact.</h1>
         </div>
       </footer>
     </main>
---
+++ src/constants/experiences.js
@@ -0,0 +1,36 @@
+const experiences = [
+    {
+        title: 'IT Engineer',
+        date: 'Nov 22 - Present',
+        company_name: 'Sustenir Group',
+        icon: '',
+        responsibilities: [
+            'Resolved general day-to-day IT issues',
+            'Document and manage cloud, on-premise and CEA infrastructure',
+            'Programming, developing or integrating various information and operational technology for the wider business',
+        ],
+    },
+    {
+        title: 'Full Stack Developer',
+        date: 'Dec 2018 - Feb 2021',
+        company_name: 'BlueBell People',
+        icon: '',
+        responsibilities: [
+            'Developed IOT web apps',
+            'Developed mobile application using ReactJS and React Native.',
+        ],
+    },
+    {
+        title: 'Junior Web Developer',
+        date: 'Sep 2018 - Jan 2021',
+        company_name: 'Host Myanmar (Software Solutions)',
+        icon: '',
+        responsibilities: [
+            'Developed HR softwares',
+            'Developed Travels and Tour web apps',
+            'Developed government related softwares using PHP and ReactJS',
+        ],
+    },
+]
+
+export default experiences;
