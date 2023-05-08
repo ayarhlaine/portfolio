d03d636426ad44579938289da51febf1271fe99d
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2023-05-08 15:51:37 +0800

    added Poppins font

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,8 @@ const App = () => {
         {/* Hero Section */}
         <div className={`${styles.padding} "hero-section mt-10 flex w-full flex-row text-secondary"`}>
           <div className="hero-left-section basis-1/2">
-            <h1 className="text-5xl mt-5 text-white-100">Hi, I&apos;m <span className='text-[#A663CC] underline underline-offset-9'>Ayar Hlaine</span></h1>
-            <p className='mt-10 text-lg text-white-100'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid dolorum velit minus laborum iusto sequi blanditiis consequatur tempore hic nam totam repellendus sunt quae, unde soluta at, culpa veniam in.</p>
+            <h1 className="text-5xl mt-5 text-white-100">Hi, I&apos;m <span className='text-5xl text-[#A663CC]'>Ayar Hlaine</span></h1>
+            <p className='mt-10 text-lg text-white-100'>I develop high quality web applications.</p>
           </div>
           <div className="hero-right-section basis-1/2 flex item-center justify-center">
             <img className='rounded-full' src='/assets/me.jpg' alt='Ayar Hlaine Photo' width={400} height={400}/>
