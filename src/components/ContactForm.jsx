d23bc8c2aa2f6f4fad822ca35610273b0f656dd2
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2023-05-11 13:32:50 +0800

    Update ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { styles } from '../styles';
 import emailjs from '@emailjs/browser';
 
@@ -8,11 +8,22 @@ const EMAIL_PUBLIC_KEY = import.meta.env.VITE_EMAIL_PUBLIC_KEY;
 
 const ContactForm = () => {
     const form = useRef();
+    const [visible, setVisible] = useState(false);
+
+    const showMessage = () => {
+        setVisible(true);
+        setTimeout(() => {
+            setVisible(false)
+        }, 5000)
+    }
+
     const sendEmail = (e) => {
         e.preventDefault();
         emailjs.sendForm(EMAIL_SERVICE_ID, EMAIL_TEMPLATE_ID, form.current, EMAIL_PUBLIC_KEY)
             .then((result) => {
-                console.log(result.text);
+                if(result.text === 'OK') {
+                    showMessage()
+                }
             }, (error) => {
                 console.log(error.text);
             });
@@ -27,11 +38,11 @@ const ContactForm = () => {
                     <form ref={form} onSubmit={sendEmail}>
                         <div className='mt-5'>
                             <label className='text-white-100'>Your Name</label>
-                            <input name='from_name' type="text" className='mt-5 p-3 w-[100%] outline-0 text-black' placeholder='What is your name?'/>
+                            <input name='name' type="text" className='mt-5 p-3 w-[100%] outline-0 text-black' placeholder='What is your name?'/>
                         </div>
                         <div className='mt-5'>
                             <label className='text-white-100'>Your Email</label>
-                            <input name='from_email' type="text" className='mt-5 p-3 w-[100%] outline-0 text-black' placeholder='What is your email?'/>
+                            <input name='email' type="text" className='mt-5 p-3 w-[100%] outline-0 text-black' placeholder='What is your email?'/>
                         </div>
                         <div className='mt-5'>
                             <label className='text-white-100'>Your Message</label>
@@ -40,6 +51,23 @@ const ContactForm = () => {
                         <div className='mt-5'>
                             <button type='submit' className='bg-teal-400 text-white py-2 px-5'>Send</button>
                         </div>
+                        {
+                            visible && <div className='mt-3'>
+                               <div className="flex p-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50" role="alert">
+                                    <svg aria-hidden="true" 
+                                    className="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" 
+                                    xmlns="http://www.w3.org/2000/svg">
+                                        <path fillRule="evenodd" 
+                                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" 
+                                        clipRule="evenodd"></path>
+                                    </svg>
+                                    <span className="sr-only">Info</span>
+                                    <div>
+                                        <span className="font-medium">Success!</span> Well received your message! I will get back to you as soon as possible.
+                                    </div>
+                                </div>
+                            </div>
+                        }
                     </form>
                 </div>
         </div>
